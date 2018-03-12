cab19b383b19b08d841af42f99451b4024b2bb27
Ali Ahmad <ahmad.aligohar@icloud.com>
2018-03-12 10:44:16 -0700

    Changed recipe reducer file name

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { combineReducers } from 'redux';
-import recipeReducer from './recipe-reducer';
+import recipeReducer from './recipe_reducer';
 import { reducer as formReducer } from 'redux-form';
 
 const rootReducer = combineReducers({
